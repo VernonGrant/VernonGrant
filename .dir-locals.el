dabcf39326eb730750bb6320d4a63a58b4068080
Vernon Grant <vernon@twothreebird.com>
2023-09-08 15:37:36 +0200

    Adds favicons

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+(
+ ;; Project Tasks
+ (nil . ((vg-project-tasks . (
+                              ("Open Site" . (lambda()
+                                                   (vg-shell-command-in-project-root "open ./docs/index.html")
+                                                   )))))))
