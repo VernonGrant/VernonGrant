551394aaf506842bb499d36b54d301b1f18dacf7
Vernon Grant <vernon@twothreebird.com>
2023-09-09 17:39:36 +0200

    Updates domains

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
  ;; Project Tasks
  (nil . ((vg-project-tasks . (
                               ("Open Site" . (lambda()
-                                                   (vg-shell-command-in-project-root "open ./docs/index.html")
-                                                   )))))))
+                                               (vg-shell-command-in-project-root "open ./docs/index.html")
+                                               )))))))
