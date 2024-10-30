6d1ca98290ee4142993071fd13acacf9a1d55a44
Vernon Grant <vernon@twothreebird.com>
2024-10-30 20:04:34 +0200

    Adds new blog

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-(
- ;; Project Tasks
- (nil . ((vg-project-tasks . (
-                              ("Regen RSS" . (lambda()
-                                               (vg-multi-term-shell-command "./gen-rss.lisp" t)
-                                               ))
-                              ("Open Site" . (lambda()
-                                               (vg-multi-term-shell-command "open ./docs/index.html" t)
-                                               )))))))
---
+++ content/2024-10-30-hello-world.md
@@ -0,0 +1,5 @@
++++
+title = "Say, Hello World!"
++++
+
+**I'm turning over a new leaf**, leaving some things in the past and getting serious about my carreer and life. This blog is where I'll record my thoughts on things I'm interested in.
